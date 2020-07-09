78c6b7ccac807df62b6ad04016e073252b39e88e
Matt Mason <masonmj10@gmail.com>
2020-07-09 14:54:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 
 
 def reverse_each_word(str)
-  new_array = str.split(" ")
-  new_array.collect {|word| word.reverse!}
-  new_array.join(" ")
+  array = str.split(" ")
+  array.collect {|word| word.reverse!}
+  array.join(" ")
 end
